1e2aac2b07a9e1ddf2d296b69d2647aad6d8d230
Will Howell <will_howell@Wills-MacBook-Pro.local>
2021-03-11 15:56:03 -0500

    worked out bottom bar

--- client/src/components/layout/TopBar.js
+++ client/src/components/layout/TopBar.js
@@ -1,25 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import SignOutButton from "../authentication/SignOutButton";
 
-const TopBar = ({ user }) => {
-  const unauthenticatedListItems = [
-    <li key="sign-in">
-      <Link to="/user-sessions/new">Sign In</Link>
-    </li>,
-    <li key="sign-up">
-      <Link to="/users/new" className="button">
-        Sign Up
-      </Link>
-    </li>,
-  ];
-
-  const authenticatedListItems = [
-    <li key="sign-out">
-      <SignOutButton />
-    </li>,
-  ];
-
+const TopBar = () => {
   return (
     <div className="top-bar">
       <div className="top-bar-left">
